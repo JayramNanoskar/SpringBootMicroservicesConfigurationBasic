198e8302293c51d2de5fd2847372bcee292f7d40
JayramNanoskar <jayram.n978@gmail.com>
2021-11-25 02:03:45 +0530

    Using ConfigurationProperties config with Spring Boot

--- spring-boot-config/src/main/java/com/jayram/springbootconfig/controller/GreetingController.java
+++ spring-boot-config/src/main/java/com/jayram/springbootconfig/controller/GreetingController.java
@@ -1,12 +1,18 @@
 package com.jayram.springbootconfig.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jayram.springbootconfig.util.DbSetting;
+
 @RestController
 public class GreetingController {
 
+	@Autowired
+	DbSetting dbSetting;
+	
 	@Value("${my.greeting}")
 	private String greetingMessage;
 	
@@ -16,5 +22,10 @@ public class GreetingController {
 	@GetMapping("/greeting")
 	public String greeting() {
 		return greetingMessage+" "+description;
+	}
+	
+	@GetMapping("/getDbSetting")
+	public String getDbSetting() {
+		return dbSetting.getConnection()+" "+dbSetting.getHost()+" "+dbSetting.getPort();
 	}
 }
